b503369dc0974b68cbf8440ffd1fa5a4b99f996b
Gustavo Gois <gustavo.gois@gmail.com>
2019-08-18 11:23:20 +0100

    Initial commit

---
+++ src/main/java/gois/study/tdjd/TdjdApplication.java
@@ -0,0 +1,13 @@
+package gois.study.tdjd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdjdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdjdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gois/study/tdjd/TdjdApplicationTests.java
@@ -0,0 +1,16 @@
+package gois.study.tdjd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TdjdApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
