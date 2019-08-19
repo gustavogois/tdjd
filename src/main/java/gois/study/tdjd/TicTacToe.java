f2d7e567231ba9fe64c5ddb3e301e13ac343ebc6
Gustavo Gois <gustavo.gois@gmail.com>
2019-08-20 00:09:21 +0100

    Related with requirement #1 Test case: When a piece is placed anywhere outside the x-axis, then RuntimeException is thrown
    Green phase

--- src/main/java/gois/study/tdjd/TicTacToe.java
+++ src/main/java/gois/study/tdjd/TicTacToe.java
@@ -1,7 +1,9 @@
 package gois.study.tdjd;
 
 public class TicTacToe {
-    public void play(int i, int i1) {
-
+    public void play(int x, int y) {
+        if (x < 1 || x > 3) {
+            throw new RuntimeException("X is outside board");
+        }
     }
 }
