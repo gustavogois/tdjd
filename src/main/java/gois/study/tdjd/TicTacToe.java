9f36f6d99153530fc8ce8d4f7e6497c6adc238cc
Gustavo Gois <gustavo.gois@gmail.com>
2019-08-20 00:21:32 +0100

    Related with requirement #1 Test case: When a piece is placed on an occupied space, then RuntimeException is thrown.
    Green phase
    
    Keep in mind that we're still not storing who played (X or O)

--- src/main/java/gois/study/tdjd/TicTacToe.java
+++ src/main/java/gois/study/tdjd/TicTacToe.java
@@ -1,11 +1,26 @@
 package gois.study.tdjd;
 
 public class TicTacToe {
+
+    private Character[][] board = {
+            {'\0', '\0', '\0'},
+            {'\0', '\0', '\0'},
+            {'\0', '\0', '\0'}
+    };
+
     public void play(int x, int y) {
+
         if (x < 1 || x > 3) {
             throw new RuntimeException("X is outside board");
         } else if (y < 1 || y > 3) {
             throw new RuntimeException("Y is outside board");
         }
+
+        if (board[x-1][y-1] != '\0') {
+            throw new RuntimeException("Box is occupied");
+        } else {
+            board[x-1][y-1] = 'X';
+        }
+
     }
 }
