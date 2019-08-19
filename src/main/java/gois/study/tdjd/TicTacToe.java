0968808b43be6b7852a457ec1dc1d9bed470904c
Gustavo Gois <gustavo.gois@gmail.com>
2019-08-20 00:29:51 +0100

    Closes #1 Refactoring With this refactoring, we did not change the functionality of the play method. It behaves exactly the same as it behaved before, but the new code is a bit more readable. Since we had tests that covered all the existing functionality, there was no fear that we might do something wrong. As long as all tests are passing all the time and refactoring did not introduce any new behavior, it is safe to make changes to the code.

--- src/main/java/gois/study/tdjd/TicTacToe.java
+++ src/main/java/gois/study/tdjd/TicTacToe.java
@@ -10,17 +10,23 @@ public class TicTacToe {
 
     public void play(int x, int y) {
 
-        if (x < 1 || x > 3) {
-            throw new RuntimeException("X is outside board");
-        } else if (y < 1 || y > 3) {
-            throw new RuntimeException("Y is outside board");
-        }
+        checkAxis(x);
+        checkAxis(y);
+        setBox(x, y);
 
+    }
+
+    private void setBox(int x, int y) {
         if (board[x-1][y-1] != '\0') {
             throw new RuntimeException("Box is occupied");
         } else {
             board[x-1][y-1] = 'X';
         }
+    }
 
+    private void checkAxis(int axis) {
+        if (axis < 1 || axis > 3) {
+            throw new RuntimeException("Axis is outside board");
+        }
     }
 }
