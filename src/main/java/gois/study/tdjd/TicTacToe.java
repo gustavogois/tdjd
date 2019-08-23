50d332254998fc487be04bd742c5643b5c704bbb
Gustavo Gois <gustavo.gois@gmail.com>
2019-08-23 22:09:03 +0100

    Related to #3 To fulfill this test, we need to check whether any horizontal line is filled by the same mark as the current player. Until this moment, we didn't care what was put on the board array. Now, we need to introduce not only which board boxes are empty, but also which player played them

--- src/main/java/gois/study/tdjd/TicTacToe.java
+++ src/main/java/gois/study/tdjd/TicTacToe.java
@@ -18,16 +18,23 @@ public class TicTacToe {
 
         checkAxis(x);
         checkAxis(y);
-        setBox(x, y);
         lastPlayer = nextPlayer();
+        setBox(x, y, lastPlayer);
+        for (int index = 0; index < 3; index++) {
+            if (board[0][index] == lastPlayer
+                    && board[1][index] == lastPlayer
+                    && board[2][index] == lastPlayer) {
+                return lastPlayer + " is the winner";
+            }
+        }
         return "No winner";
     }
 
-    private void setBox(int x, int y) {
-        if (board[x-1][y-1] != BLANK) {
+    private void setBox(int x, int y, char lastPlayer) {
+        if (board[x - 1][y - 1] != '\0') {
             throw new RuntimeException("Box is occupied");
         } else {
-            board[x-1][y-1] = PLAYER_X;
+            board[x - 1][y - 1] = lastPlayer;
         }
     }
 
