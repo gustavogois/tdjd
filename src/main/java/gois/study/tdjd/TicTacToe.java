eeb8333c85b49ad92e35b37a39ffeaa5755df188
Gustavo Gois <gustavo.gois@gmail.com>
2019-08-24 10:18:33 +0100

    The preceding code satisfies the tests, but is not necessarily the final version. It served its purpose of getting code coverage as quickly as possible. Now, since we have tests that guarantee the integrity of the expected behavior, we can refactor the code
    This refactored solution looks better. The play method keeps being short and easy to understand. Winning logic is moved to a separate method. Not only have we kept the play method's purpose clear, but this separation also allows us to grow the winning condition's code in separation from the rest.

--- src/main/java/gois/study/tdjd/TicTacToe.java
+++ src/main/java/gois/study/tdjd/TicTacToe.java
@@ -5,6 +5,7 @@ public class TicTacToe {
     public static final char BLANK = '\0';
     public static final char PLAYER_X = 'X';
     public static final char PLAYER_O = 'O';
+    private static final int SIZE = 3;
 
     private char lastPlayer = BLANK;
 
@@ -20,14 +21,16 @@ public class TicTacToe {
         checkAxis(y);
         lastPlayer = nextPlayer();
         setBox(x, y, lastPlayer);
-        for (int index = 0; index < 3; index++) {
-            if (board[0][index] == lastPlayer
-                    && board[1][index] == lastPlayer
-                    && board[2][index] == lastPlayer) {
-                return lastPlayer + " is the winner";
+        return isWin() ? lastPlayer + " is the winner" : "No winner";
+    }
+
+    private boolean isWin() {
+        for (int i = 0; i < SIZE; i++) {
+            if (board[0][i] + board[1][i] + board[2][i] == (lastPlayer * SIZE)) {
+                return true;
             }
         }
-        return "No winner";
+        return false;
     }
 
     private void setBox(int x, int y, char lastPlayer) {
