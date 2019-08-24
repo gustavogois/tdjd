d4a398d819684da1d68adfa30665e047f1944d7d
Gustavo Gois <gustavo.gois@gmail.com>
2019-08-24 11:03:36 +0100

    Closes #3 Refactoring Better readability

--- src/main/java/gois/study/tdjd/TicTacToe.java
+++ src/main/java/gois/study/tdjd/TicTacToe.java
@@ -31,12 +31,19 @@ public class TicTacToe {
         for (int i = 0; i < SIZE; i++) {
             diagonal1 += board[i][i];
             diagonal2 += board[i][SIZE - i - 1];
-            if ( (board[0][i] + board[1][i] + board[2][i] == playerTotal) ||
-                    (board[i][0] + board[i][1] + board[i][2] == playerTotal)) {
+            if ( columnPlayerWin(playerTotal, i) || linePlayerWin(playerTotal, board[i])) {
                 return true;
             }
         }
         return (diagonal1 == playerTotal || diagonal2 == playerTotal) ? true : false;
+    }
+
+    private boolean linePlayerWin(int playerTotal, Character[] characters) {
+        return characters[0] + characters[1] + characters[2] == playerTotal;
+    }
+
+    private boolean columnPlayerWin(int playerTotal, int i) {
+        return board[0][i] + board[1][i] + board[2][i] == playerTotal;
     }
 
     private void setBox(int x, int y, char lastPlayer) {
