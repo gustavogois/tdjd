ae372a16447049470fa57ff4f9e98a4355e9a1ad
Gustavo Gois <gustavo.gois@gmail.com>
2019-08-24 10:21:06 +0100

    Relates to #3 We should also check whether there is a win by filling the vertical line

--- src/test/java/gois/study/tdjd/TicTacToeSpec.java
+++ src/test/java/gois/study/tdjd/TicTacToeSpec.java
@@ -65,4 +65,15 @@ public class TicTacToeSpec {
         assertEquals("X is the winner", actual);
     }
 
+    @Test
+    public void whenPlayAndWholeVerticalLineThenWinner() {
+        ticTacToe.play(2, 1); // X
+        ticTacToe.play(1, 1); // O
+        ticTacToe.play(3, 1); // X
+        ticTacToe.play(1, 2); // O
+        ticTacToe.play(2, 2); // X
+        String actual = ticTacToe.play(1, 3); // O
+        assertEquals("O is the winner", actual);
+    }
+
 }
