117b644d6369159b1d2ee9720700e533e888787e
Gustavo Gois <gustavo.gois@gmail.com>
2019-09-11 11:21:25 +0100

    Testing – adding operation feedback Let us change the return type of our saveMove method to boolean

--- src/test/java/gois/study/tdjd/business/TicTacToeBusinessTest.java
+++ src/test/java/gois/study/tdjd/business/TicTacToeBusinessTest.java
@@ -4,6 +4,7 @@ import gois.study.tdjd.model.TicTacToe;
 import gois.study.tdjd.repository.TicTacToeRepository;
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
 public class TicTacToeBusinessTest {
@@ -18,5 +19,14 @@ public class TicTacToeBusinessTest {
         business.saveMove(bean);
 
         verify(repository, times(1)).save(bean);
+    }
+
+    @Test
+    public void whenSaveMoveThenReturnTrue() {
+        TicTacToeRepository repository = mock(TicTacToeRepository.class);
+        TicTacToeBusiness business = new TicTacToeBusiness(repository);
+
+        TicTacToe bean = new TicTacToe(3, 2, 1, 'Y');
+        assertTrue(business.saveMove(bean));
     }
 }
